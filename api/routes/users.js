a0d81cd2b9efe88aa19b918f67b4401249e6457b
Bernardo Marques <bernardohmarques@hotmail.com>
2020-12-22 12:50:52 -0300

    feat: created DELETE method to delete an user using his id

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,9 @@ module.exports = app => {
     const controller = app.controllers.users
 
     app.route('/users/')
-    .get(controller.listUsers)
-    .post(controller.saveUsers)
+        .get(controller.listUsers)
+        .post(controller.saveUsers)
+
+    app.route('/users/:userId')
+        .delete(controller.removeUsers)
 }
